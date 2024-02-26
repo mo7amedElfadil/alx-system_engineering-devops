0ed008d0f5394d8e9f4e9dc0fb28b9b03d0d8787
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-02-26 21:09:59 +0200

    initial commit

---
+++ 0x0C-web_server/main_files/0-main.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# executes file 0-transfer_file with args
+path=$(dirname "$(dirname "$0")")/0-transfer_file
+FILE=some_page.html
+IP=54.144.238.161
+USER=ubuntu
+path_to_ssh_key=~/.ssh/id_rsa
+
+echo "checking if file exists remotely"
+ssh -i "$path_to_ssh_key" "$USER@$IP" "test -f ~/$FILE"
+if [ $? -eq 0 ]; then
+	echo "file exists remotely"
+	exit 0
+else
+	echo "file does not exist remotely"
+fi
+
+echo "Checking if file exists locally"
+if [ -f $FILE ]; then
+	echo "file exists locally"
+else
+	echo "file does not exist locally"
+	echo "Creating file locally"
+	touch $FILE
+fi
+
+
+echo "Executing $path with arguments"
+"$path" "$FILE" "$IP" "$USER" "$path_to_ssh_key"
+if [ $? -eq 0 ]; then
+	echo "File transferred successfully"
+else
+	echo "File transfer failed"
+	exit 1
+fi
+
+echo "Checking if file exists remotely again"
+ssh -i "$path_to_ssh_key" "$USER@$IP" "test -f ~/$FILE"
+if [ $? -eq 0 ]; then
+	echo "file exists remotely"
+	ssh -i $path_to_ssh_key $USER@$IP 'ls ~/'
+else
+	echo "file does not exist remotely"
+	ssh -i $path_to_ssh_key $USER@$IP 'ls ~/'
+	exit 1
+fi
+
+
---
+++ 0x0C-web_server/main_files/1-main.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# this script transfers and runs a shell script that installs nginx on a web server
+
+path_to_install=$(dirname "$(dirname "$0")")/1-install_nginx_web_server
+path=$(dirname "$(dirname "$0")")/0-transfer_file
+installer=1-install_nginx_web_server
+IP=54.144.238.161
+USER=ubuntu
+path_to_ssh_key=~/.ssh/id_rsa
+
+echo "Executing $path with arguments"
+"$path" "$installer" "$IP" "$USER" "$path_to_ssh_key"
+if [ $? -eq 0 ]; then
+    echo "File transferred successfully"
+else
+    echo "File transfer failed"
+    exit 1
+fi
+
+echo "Checking if file exists remotely"
+ssh -i "$path_to_ssh_key" "$USER@$IP" "test -f ~/$installer"
+if [ $? -eq 0 ]; then
+    echo "file exists remotely. Executing the file..."
+    ssh -i $path_to_ssh_key $USER@$IP "./$installer"
+else
+    echo "file does not exist remotely"
+    ssh -i $path_to_ssh_key $USER@$IP 'ls ~/'
+    exit 1
+fi
+
+if curl -s "$IP" | grep -q "Hello World!"; then
+	echo "nginx is installed and running"
+else
+	echo "nginx is not running"
+	exit 1
+fi
