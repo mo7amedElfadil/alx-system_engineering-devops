29ec2311776db7694a8507118321645d503728f5
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-02-27 08:30:19 +0200

    added class

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,41 @@
 # install nginx
 class nginx {
-package { 'nginx':
-  ensure => 'installed',
+  package { 'nginx':
+    ensure => 'installed',
+  }
+  # create a file with content
+  file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hello world!
+	',
+    require => Package['nginx'],
+  }
+
+  # modify the default nginx configuration
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => "
+    server {
+		listen 80 default_server;
+		listen [::]:80 default_server;
+		root /var/www/html;
+		index index.html;
+		server_name _;
+		location / {
+			try_files \$uri \$uri/ =404;
+		}
+		location /redirect_me {
+			return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+		}
+	  }
+  ",
+    require => Package['nginx'],
+  }
+  service {'nginx':
+    ensure    => 'running',
+    enable    => true,
+    subscribe => File['/etc/nginx/sites-available/default'],
+  }
 }
 
-# create a file with content
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello world!',
-  require => Package['nginx'],
-}
-# modify the default nginx configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-  server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /var/www/html;
-	index index.html;
-	server_name _;
-	location / {
-		try_files \$uri \$uri/ =404;
-	}
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-}
-",
-  require => Package['nginx'],
-}
-service {'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
-}
-}
 include nginx
