43369ea99e95a25e72124e3868124dfc39d60506
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-02-27 07:43:52 +0200

    7 updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,21 +3,20 @@ package { 'nginx':
   ensure => 'installed',
 }
 
+# create a file with content
 file { '/var/www/html/index.html':
   ensure  => file,
   content => 'Hello world!',
   require => Package['nginx'],
   mode    => '0644',
 }
+# modify the default nginx configuration
 $new_string = "location /redirect_me {\\
 		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\\
 }"
-
 exec { 'redirect_me':
   path    => ['/bin', '/usr/bin'],
   command => "sudo sed -i '/server_name _;/a ${new_string}' /etc/nginx/sites-available/default",
-  require => Service['nginx'],
-  notify  => Service['nginx'],
 }
 service {'nginx':
   ensure  => running,
