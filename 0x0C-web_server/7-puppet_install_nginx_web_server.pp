11da312238b0339ce34fe8c4c8299446023fdf0b
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-02-27 08:45:36 +0200

    mohannad

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,4 @@
-# install nginx
+# This class manages the installation and configuration of Nginx
 class nginx {
   package { 'nginx':
     ensure => installed,
@@ -6,32 +6,31 @@ class nginx {
 
   file { '/var/www/html/index.html':
     ensure  => file,
-    content => 'Hello world!',
+    content => 'Hello World!',
     require => Package['nginx'],
   }
 
-  # modify the default nginx configuration
   file { '/etc/nginx/sites-available/default':
     ensure  => file,
     content => "
-	  server {
-		listen 80 default_server;
-		listen [::]:80 default_server;
-		root /var/www/html;
-		index index.html;
-		server_name _;
-		location / {
-			try_files \$uri \$uri/ =404;
-		}
-		location /redirect_me {
-			return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-		}
-	  }
-  ",
+      server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+        index index.html;
+        server_name _;
+        location / {
+          try_files \$uri \$uri/ =404;
+        }
+        location /redirect_me {
+          return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+      }
+    ",
     require => Package['nginx'],
   }
 
-  service {'nginx':
+  service { 'nginx':
     ensure    => running,
     enable    => true,
     subscribe => File['/etc/nginx/sites-available/default'],
