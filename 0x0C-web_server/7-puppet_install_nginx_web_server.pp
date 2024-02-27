1f1009589daf568b7052dd502a7a44167d7c8972
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-02-27 07:49:13 +0200

    7 changed to explicit file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,9 +14,23 @@ file { '/var/www/html/index.html':
 $new_string = "location /redirect_me {\\
 		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\\
 }"
-exec { 'redirect_me':
-  path    => ['/bin', '/usr/bin'],
-  command => "sudo sed -i '/server_name _;/a ${new_string}' /etc/nginx/sites-available/default",
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+	location / {
+		try_files \$uri \$uri/ @404;
+	}
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+}",
+  mode    => '0644',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 service {'nginx':
   ensure  => 'running',
