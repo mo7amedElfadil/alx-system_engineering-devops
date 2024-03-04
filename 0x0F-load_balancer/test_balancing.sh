99f0b5491da5c53287e65c4aa9bb5a3399ef1ff5
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-05 00:18:12 +0200

    added general back

--- 0x0F-load_balancer/test_balancing.sh
+++ 0x0F-load_balancer/test_balancing.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 # run loop 10 times
 echo "Server 1"
-for _ in {1..10}
+for _ in {1..5}
 do
 	echo "$(curl -sI "$SERVER1" | grep "X-Served-By")"
 done
 echo
 echo "Server 2"
-for _ in {1..10}
+for _ in {1..5}
 do
 	echo "$(curl -sI "$SERVER2" | grep "X-Served-By")"
 done
-echo "Server 3 LOAD BALANCER"
 
+echo
+echo "Server 3 LOAD BALANCER"
 for _ in {1..10}
 do
-	echo "$(curl -sI "$SERVER3LB" | grep "X-Served-By")"
+	res=$(curl -sI "$SERVER3LB")
+	if echo "$res" | grep -q "X-Served-By"; then
+		res=$(echo "$res" | grep  "X-Served-By")
+		echo -e "${COLOR_GREEN}$res ${RESET}"
+	else
+		res=$(echo "$res" | grep  "x-served-by")
+		echo -e "${COLOR_RED}$res ${RESET}"
+	fi
 done
