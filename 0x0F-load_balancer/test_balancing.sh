f57126ffbe92611c7cd8edb6f01dbeebf27bcc49
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-04 23:21:47 +0200

    idk

--- 0x0F-load_balancer/test_balancing.sh
+++ 0x0F-load_balancer/test_balancing.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 # run loop 10 times
+echo "Server 1"
+for _ in {1..10}
+do
+	echo "$(curl -sI "$SERVER1" | grep "X-Served-By")"
+done
+echo
+echo "Server 2"
+for _ in {1..10}
+do
+	echo "$(curl -sI "$SERVER2" | grep "X-Served-By")"
+done
+echo "Server 3 LOAD BALANCER"
+
 for _ in {1..10}
 do
 	echo "$(curl -sI "$SERVER3LB" | grep "X-Served-By")"
