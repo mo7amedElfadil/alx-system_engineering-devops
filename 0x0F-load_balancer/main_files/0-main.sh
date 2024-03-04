e0accc12ba2f6592fce16c935d2ad1fbe245e341
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-04 20:23:04 +0200

    init commit

--- 0x0F-load_balancer/main_files/0-main.sh
+++ 0x0F-load_balancer/main_files/0-main.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
-# this script transfers and runs a shell script that installs nginx on a web server
 
-path=$(dirname "$(dirname "$0")")/0-transfer_file
-installer=3-redirection
-IP=$SERVER
+path=$(dirname "$(dirname "$0")")/transfer_file
+installer=0-custom_http_response_header
+if [ "$#" -eq 1 ]; then
+	if [ "$1" == 1 ]; then
+		IP=$SERVER1
+	else
+		IP=$SERVER2
+	fi
+else
+	IP=$SERVER2
+fi
 USER=ubuntu
 path_to_ssh_key=~/.ssh/id_rsa
 
-echo "Executing $path with arguments"
+echo "Executing $path with arguments on server $IP"
 "$path" "$installer" "$IP" "$USER" "$path_to_ssh_key"
 if [ $? -eq 0 ]; then
     echo "File transferred successfully"
@@ -25,15 +32,14 @@ if [ $? -eq 0 ]; then
 	else
 		read -p "install? (y/n) " -n 1 -r
 	fi
-
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		ssh -i "$path_to_ssh_key" "$USER"@"$IP" "./$installer; rm -f ~/$installer"
+		ssh -i $path_to_ssh_key $USER@$IP "./$installer; rm -f ~/$installer"
 	else
 		echo "file exists remotely. Not executing the file"
 	fi
 else
     echo "file does not exist remotely"
-    ssh -i "$path_to_ssh_key" "$USER"@"$IP" 'ls ~/'
+    ssh -i $path_to_ssh_key $USER@$IP 'ls ~/'
     exit 1
 fi
 
@@ -54,4 +60,23 @@ if curl_output=$(curl -sI "$IP/redirect_me" | head -n 1); then
 else
     echo "Failed to retrieve HTTP response"
     exit 1
+fi
+
+if curl_output=$(curl -sI "$IP/not_found" | head -n 1); then
+	if echo "$curl_output" | grep -q "HTTP/1.1 404 Not Found"; then
+		echo "404 is working"
+	else
+		echo "Expected 404 status not found in the first line of HTTP response"
+		exit 1
+	fi
+else
+	echo "Failed to retrieve HTTP response"
+	exit 1
+fi
+
+if curl_output=$(curl -sI "$IP" | grep "X-Served-By"); then
+	echo "X-Served-By header is present"
+else
+	echo "X-Served-By header is not present"
+	exit 1
 fi
