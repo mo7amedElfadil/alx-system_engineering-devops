67232ab6ea2bbb73baba4d63a84d7d845184d6ab
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-05 02:06:45 +0200

    updated explicit

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,20 +8,23 @@ package { 'nginx':
   }
 file { '/etc/nginx/sites-available/default':
     ensure  => file,
-    content => "
-server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
-  root /var/www/html;
-  index index.html;
-  server_name _;
-  location / {
-   add_header X-Served-By ${hostname};
-    try_files \$uri \$uri/ =404;
-  }
-  location /redirect_me {
-    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-  }
+    content => "server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html;
+	server_name _;
+	location / {
+		add_header X-Served-By ${hostname};
+		try_files $uri $uri/ =404;
+	}
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+	error_page 404 /404.html;
+	location = /404.html {
+		internal;
+	}
 }
 ",
     require => Package['nginx'],
