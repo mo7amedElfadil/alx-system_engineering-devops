feb332ddbfd56efe454adc704ae8f7431110e02d
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-05 02:13:23 +0200

    updated

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,13 +9,13 @@ package { 'nginx':
 file { '/etc/nginx/sites-available/default':
     ensure  => file,
     content => "server {
+	add_header X-Served-By ${hostname};
 	listen 80 default_server;
 	listen [::]:80 default_server;
 	root /var/www/html;
 	index index.html;
 	server_name _;
 	location / {
-		add_header X-Served-By ${hostname};
 		try_files $uri $uri/ =404;
 	}
 	location /redirect_me {
