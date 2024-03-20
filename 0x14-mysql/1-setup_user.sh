133b2a66f70c6f1e8338578d7a25add768843e02
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-20 02:43:12 +0200

    replica config

--- 0x14-mysql/1-setup_user.sh
+++ 0x14-mysql/1-setup_user.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+# this script sets up a new user for mysql
+
+USER="holberton_user"
+HOST="localhost"
+PASSWORD="projectcorrection280hbtn"
+
+sudo mysql -u root -e "CREATE USER IF NOT EXISTS '${USER}'@'${HOST}' IDENTIFIED BY '${PASSWORD}';"
+sudo mysql -u root -e "GRANT REPLICATION CLIENT ON *.* TO '${USER}'@'${HOST}';"
+sudo mysql -u root -e "FLUSH PRIVILEGES;"
