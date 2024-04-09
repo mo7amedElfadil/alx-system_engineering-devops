9fc85ffe8e33242f9242373eaaf4aaa3a3f3d02f
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-04-09 22:02:57 +0200

    try kind

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,13 +9,14 @@ def top_ten(subreddit):
     queries the Reddit API and prints the titles of the first 10 hot posts
     listed for a given subreddit.
     """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             for post in response.json().get("data").get("children"):
-                print(post.get("data").get("title"))
+                if post.get("kind") == "t3":
+                    print(post.get("data").get("title"))
             return
     except (KeyError, ValueError):
         print(None)
