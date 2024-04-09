6fbcb9b6a841e2539527309680d4bcccbf834730
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-04-09 20:47:53 +0200

    fixed none condition

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,6 +13,7 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers={'User-Agent': 'mo7amed'})
-    if response.status_code != 200:
+    if (response.status_code != 200
+            or response.json().get('data').get('dist') == 0):
         return 0
     return response.json().get('data').get('subscribers')
