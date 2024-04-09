3c90d5a740ad683aa47889b759a047df4d20dd85
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-04-09 21:03:01 +0200

    replace redirects with try except

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,8 +12,11 @@ def number_of_subscribers(subreddit):
     the function should return 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers={"User-Agent": "mo7amed/1.0"},
-            allow_redirects=False)
+    response = requests.get(url, headers={"User-Agent": "mo7amed/1.0"})
     if response.status_code != 200:
         return 0
-    return response.json()["data"]["subscribers"]
+    try:
+        result = response.json()["data"]["subscribers"]
+        return result
+    except (KeyError, ValueError):
+        return 0
