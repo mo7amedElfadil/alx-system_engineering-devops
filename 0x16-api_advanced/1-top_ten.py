4edb8d88efd699d7c0cdfef8e8767636924d3ed3
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-04-09 21:57:38 +0200

    allow red

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,7 +12,7 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             for post in response.json().get("data").get("children"):
                 print(post.get("data").get("title"))
