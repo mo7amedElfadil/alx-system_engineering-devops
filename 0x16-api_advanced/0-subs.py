d7ce84e8b75983c20c3d2adfb339725d08dfe3a4
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-04-09 20:56:10 +0200

    no get

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,8 +12,8 @@ def number_of_subscribers(subreddit):
     the function should return 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers={'User-Agent': 'mo7amed'},
+    response = requests.get(url, headers={'User-Agent': 'mo7amed/0.0.1'},
             allow_redirects=False)
     if response.status_code != 200:
         return 0
-    return response.json().get('data').get('subscribers')
+    return response.json()['data']['subscribers']
