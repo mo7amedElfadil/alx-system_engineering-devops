1af5f75a5efcf82d32b52ead16c24431216007b6
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-04-18 18:26:10 +0200

    t

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,17 @@
+file { '/etc/default/nginx':
+  ensure => present,
+  notify => Exec['set_nginx_ulimit'],
+}
+
+exec { 'set_nginx_ulimit':
+  command   => '/bin/sed -i "s/^ULIMIT=.*/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  subscribe => File['/etc/default/nginx'],
+  notify    => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  subscribe  => Exec['set_nginx_ulimit'],
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+file { '/etc/security/limits.conf':
+  ensure => present,
+  notify => Exec['soft limit', 'hard limit'],
+}
+
+exec { 'soft limit':
+  command  => "/bin/sed -i 's/^holberton soft nofile.*/holberton soft nofile 65536/' /etc/security/limits.conf",
+}
+
+exec { 'hard limit':
+  command  => "/bin/sed -i 's/^holberton hard nofile.*/holberton hard nofile 65536/' /etc/security/limits.conf",
+}
